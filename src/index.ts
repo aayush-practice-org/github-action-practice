fb54dd5e35c2950b80ca270f59911d4a9e150e62
Aayush ghimire <aayushghimire@Aayush-Mac-Air.local>
2026-06-01 11:35:22 +0545

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express  from 'express';
+import type { Request, Response } from "express";
+
+const app = express();
+const PORT = 7000;
+
+app.use(express.json());
+
+app.get("/post", async (req: Request, res: Response): Promise<void> => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    res.send(data);
+});
+
+app.get("/post/:id", async (req: Request, res: Response): Promise<void> => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${req.params.id}`);
+    const data = await response.json();
+    res.send(data);
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on port: ", PORT);
+})
