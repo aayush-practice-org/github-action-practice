d953a138de6606782ddd46de096f80d873e15600
Aayush ghimire <aayushghimire@Aayush-Mac-Air.local>
2026-06-09 11:09:56 +0545

    comimit

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
+
+console.log("Port is :", PORT
 app.get("/post", async (req: Request, res: Response): Promise<void> => {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     const data = await response.json();
